7812300d7f287a4efb77b646b43c4e43ea382074
XYCKU <48044503+XYCKU@users.noreply.github.com>
2022-02-16 20:19:31 +0300

    Fix unnecessary zero-length array allocation in FileCabinetService.cs

---
+++ FileCabinetApp/FileCabinetService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetService
+    {
+        private readonly List<FileCabinetRecord> _list = new List<FileCabinetRecord>();
+
+        public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+
+        public FileCabinetRecord[] GetRecords()
+        {
+            // TODO: добавьте реализацию метода
+            return Array.Empty<FileCabinetRecord>();
+        }
+
+        public int GetStat()
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+    }
+}
