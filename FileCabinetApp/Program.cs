a01173bce26776449863cacef33cb5da001cc525
XYCKU <48044503+XYCKU@users.noreply.github.com>
2022-02-24 13:55:15 +0300

    Add ReadInput<T>

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -351,6 +351,35 @@ namespace FileCabinetApp
             _ => new FileCabinetService(new DefaultValidator()),
         };
 
+        private static T ReadInput<T>(Func<string, Tuple<bool, string, T>> converter, Func<T, Tuple<bool, string>> validator)
+        {
+            do
+            {
+                T value;
+
+                var input = Console.ReadLine() ?? string.Empty;
+                var conversionResult = converter(input);
+
+                if (!conversionResult.Item1)
+                {
+                    Console.WriteLine($"Conversion failed: {conversionResult.Item2}. Please, correct your input.");
+                    continue;
+                }
+
+                value = conversionResult.Item3;
+
+                var validationResult = validator(value);
+                if (!validationResult.Item1)
+                {
+                    Console.WriteLine($"Validation failed: {validationResult.Item2}. Please, correct your input.");
+                    continue;
+                }
+
+                return value;
+            }
+            while (true);
+        }
+
         private static void ProcessArguments(string[] args)
         {
             if (args == null)
