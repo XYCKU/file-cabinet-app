e1421abce9357dbdb8f55c226497e7d2061ae78d
XYCKU <48044503+XYCKU@users.noreply.github.com>
2022-02-25 13:59:08 +0300

    Move FileCabinetService to FileCabinetMemoryService. Add FileCabinetFileSystemService.

---
+++ FileCabinetApp/FileCabinetFilesystemService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace FileCabinetApp
+{
+    /// <inheritdoc/>
+    public class FileCabinetFilesystemService : IFileCabinetService
+    {
+        private readonly FileStream fileStream;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileCabinetFilesystemService"/> class.
+        /// </summary>
+        /// <param name="fileStream"><see cref="FileStream"/>.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="fileStream"/> is <c>null</c>.</exception>
+        public FileCabinetFilesystemService(FileStream fileStream)
+        {
+            if (fileStream is null)
+            {
+                throw new ArgumentNullException(nameof(fileStream));
+            }
+
+            this.fileStream = fileStream;
+        }
+
+        /// <inheritdoc/>
+        public int CreateRecord(FileCabinetData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void EditRecord(int id, FileCabinetData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public ReadOnlyCollection<FileCabinetRecord> FindByDateOfBirth(DateTime dateOfBirth)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public ReadOnlyCollection<FileCabinetRecord> FindByFirstName(string firstName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public ReadOnlyCollection<FileCabinetRecord> GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public int GetStat()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
